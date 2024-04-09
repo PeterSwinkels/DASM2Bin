51fa0f2d350a4e54114dee11443e86ff8e108180
Peter Swinkels <peter_swinkels@hotmail.com>
2024-04-09 21:57:33 +0200

    Add files via upload

---
+++ DASM2BIN/Core.vb
@@ -0,0 +1,73 @@
+﻿'This module's imports and settings.
+Option Compare Binary
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Convert
+Imports System.Environment
+Imports System.IO
+Imports System.Linq
+
+'This module contains this program's core procedures.
+Public Module CoreModule
+
+   'This procedure is executed when this program is started.
+   Public Sub Main()
+      Try
+         Dim Binary As New List(Of Byte)
+         Dim Code As List(Of String)
+         Dim InputPath As String = Nothing
+         Dim OutputPath As String = Nothing
+
+         If GetCommandLineArgs.Count = 2 Then
+            InputPath = GetCommandLineArgs.Last()
+            Code = New List(Of String)(From Line In File.ReadAllLines(InputPath) Select Line.Trim({ToChar(9), " "c}))
+            Code = New List(Of String)(From Line In Code Where Not Line = Nothing AndAlso Not Line.StartsWith(";"))
+            Code = New List(Of String)(From Line In Code Select Line.Substring(Line.IndexOf(" "c)).Trim())
+            Code = New List(Of String)(From Line In Code Select Line.Substring(0, Line.IndexOf(" "c)).Trim())
+
+            For Each Line As String In Code
+               Console.WriteLine(Line)
+               For Position As Integer = 0 To Line.Length - 1 Step 2
+                  Binary.Add(ToByte(Line.Substring(Position, 2), fromBase:=16))
+               Next Position
+            Next Line
+
+            OutputPath = Path.Combine(Path.GetDirectoryName(InputPath), $"{Path.GetFileNameWithoutExtension(InputPath)}.bin")
+            File.WriteAllBytes(OutputPath, Binary.ToArray())
+            Console.WriteLine($"Saved as: {OutputPath}")
+         Else
+            Console.WriteLine(ProgramInformation())
+            Console.WriteLine()
+            Console.WriteLine($"Usage: {My.Application.Info.AssemblyName} INPUT_FILE")
+         End If
+      Catch ExceptionO As Exception
+         DisplayException(ExceptionO)
+      End Try
+   End Sub
+
+   'This procedure displays any exceptions that occur.
+   Private Sub DisplayException(ExceptionO As Exception)
+      Try
+         Console.WriteLine($"ERROR: {ExceptionO.Message}")
+      Catch
+         [Exit](0)
+      End Try
+   End Sub
+
+   'This procedure returns this program's information.
+   Private Function ProgramInformation() As String
+      Try
+         With My.Application.Info
+            Return $"{ .Title} v{ .Version} - by: { .CompanyName}"
+         End With
+      Catch ExceptionO As Exception
+         DisplayException(ExceptionO)
+      End Try
+
+      Return Nothing
+   End Function
+End Module
---
+++ DASM2BIN/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DASM2Bin")>
+<Assembly: AssemblyDescription("This program saves the hexadecimal opcodes in disassembly code in binary format.")>
+<Assembly: AssemblyCompany("Peter Swinkels")>
+<Assembly: AssemblyProduct("DASM2Bin")>
+<Assembly: AssemblyCopyright("2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1eb8480a-3ff5-44a1-b1a9-0e040f4ded97")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
